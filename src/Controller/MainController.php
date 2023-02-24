c0caeda677f470f93105be6ca3bc43dd93a400ad
sgossmann <sandra.gossmann4@gmail.com>
2023-02-24 10:04:44 +0100

    ajout fichiers twigs main

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -17,4 +17,22 @@ class MainController extends AbstractController
             'accomodations' => $accomodations
         ]);
     }
+
+    #[Route('/cgu', name: 'main_cgu')]
+    public function cgu(): Response
+    {
+        return $this->render('main/cgu.html.twig');
+    }
+
+    #[Route('/about_us', name: 'main_about_us')]
+    public function aboutUs(): Response
+    {
+        return $this->render('main/about-us.html.twig');
+    }
+
+    #[Route('/legal_notice', name: 'main_legal_notice')]
+    public function legalNotice(): Response
+    {
+        return $this->render('main/legal-notice.html.twig');
+    }
 }
